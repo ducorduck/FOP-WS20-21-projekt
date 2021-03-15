da92c66d98ed599bbd376dbb2f86bb65c1c31acd
hieutran-tud <tthieu1905.hup@gmail.com>
2021-03-15 07:43:00 +0100

    Highscores View - the best I can do :)

--- src/main/fop/view/menu/HighscoreView.java
+++ src/main/fop/view/menu/HighscoreView.java
@@ -1,49 +1,74 @@
 package fop.view.menu;
 
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JEditorPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-
-import fop.controller.*;
+import javax.swing.table.DefaultTableModel;
 import fop.io.*;
 import fop.model.*;
 import java.awt.*;
-import java.awt.event.WindowEvent;
-import fop.io.IconReader;
+import java.time.format.DateTimeFormatter;
 import fop.view.MainFrame;
-import fop.view.View;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import java.util.List;
 
 public class HighscoreView extends MenuView {
 
-    public HighscoreView(MainFrame window) {
+    /**
+	 *
+	 */
+	private static final long serialVersionUID = -8498104564307083598L;
+
+	public HighscoreView(MainFrame window) {
         super(window, "Highscores");
     }
-
-
-	private static final String [] columnNames = {"Datum und Uhrzeit", "Name", "Punkte"};
-	Object [][] scoreEntries = {{"2020","A","2"},{"2020","B","1"}};
 
     @Override
     protected void addContent(JPanel contentPanel) {
         // TODO Auto-generated method stub
 		
 		//Table thing
-		JTable scoreTable = new JTable (scoreEntries, columnNames);
 		GridBagConstraints tableConstraints = new GridBagConstraints ();
 		tableConstraints.fill = GridBagConstraints.BOTH;
 		tableConstraints.insets = new Insets(0,2,2,2);
 		tableConstraints.gridx = 0;
 		tableConstraints.gridy = 0;
-		JScrollPane scroll = new JScrollPane(scoreTable);
-		scoreTable.setFillsViewportHeight(true);
-		//contentPanel.add(scroll, tableConstraints);
+
+		DefaultTableModel tableModel = new DefaultTableModel() {
+			/**
+			 *
+			 */
+			private static final long serialVersionUID = -2776817223569832037L;
+			private List<String> columnNames = List.of("Datum und Uhrzeit", "Name", "Punkte");
+			@Override
+			public String getColumnName(int columnIndex) {
+				return columnNames.get(columnIndex);
+			}
+			@Override
+			public int getColumnCount() {
+				return columnNames.size();
+			}
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};
+
+		JTable scoreTable = new JTable(tableModel);
+		scoreTable.setColumnSelectionAllowed(false);
+		scoreTable.setRowSelectionAllowed(false);
+		scoreTable.getTableHeader().setReorderingAllowed(false);
+		List<ScoreEntry> scoreEntries = ScoreEntryIO.loadScoreEntries();
+		for (ScoreEntry i : scoreEntries) {
+			String name = i.getName();
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+			String date = i.getDateTime().format(formatter);
+			String score = Integer.toString(i.getScore());
+			String[] scoreDate = {date,name,score};
+			tableModel.addRow(scoreDate);
+		}
+		contentPanel.add(new JScrollPane(scoreTable),tableConstraints);
+
         
         // back button //
 		GridBagConstraints rightImageConstraints = new GridBagConstraints();
